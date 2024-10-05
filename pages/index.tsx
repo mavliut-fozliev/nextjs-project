55b273b709e242321f8aa536f33952ea2e9dfe27
MayroYo <mayroyo.tube@gmail.com>
2024-10-06 01:19:37 +0300

    feat: add components to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import backgroundImage from "../assets/background.png";
+import styles from "../styles/Home.module.scss";
+import { Header } from "../src/components/Header";
+import { UserInputForm } from "../src/components/UserInputForm";
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.background} style={{ backgroundImage: `url(${backgroundImage.src})` }}>
+      <Header />
+      <div className={styles.content}>
+        <UserInputForm />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
