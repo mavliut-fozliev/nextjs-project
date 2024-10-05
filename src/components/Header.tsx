ebb2d47f8c211ab0bed69972ec7c6e49db7820dc
MayroYo <mayroyo.tube@gmail.com>
2024-10-06 01:18:52 +0300

    feat: create header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,35 @@
 import { NextComponentType } from "next";
 import React from "react";
+import styles from "../../styles/components/Header.module.scss";
+import Logo from "../../assets/logo.svg";
+import profile from "../../assets/profile.png";
+import Link from "next/link";
+import { useStore } from "../../store/useStore";
 
 export const Header: NextComponentType = () => {
-  return <div>Header</div>;
+  const store = useStore();
+
+  return (
+    <nav className={styles.box}>
+      <div className={styles.logo}>
+        <Logo />
+        <p className={styles.logoText}>Quantum lab</p>
+      </div>
+      <nav className={styles.menu}>
+        <Link href="./" className={styles.menuHome}>
+          Главная
+        </Link>
+        <Link href="./calculator" className={styles.menuCalculator} hidden={!store.name}>
+          Калькулятор
+        </Link>
+        <Link href="./password-generator" className={styles.menuGenerator} hidden={!store.name}>
+          Генератор паролей
+        </Link>
+      </nav>
+      <div className={styles.name}>
+        <div className={styles.nameText}>{store.name}</div>
+        <img src={profile.src} alt="profile" className={styles.nameImage} />
+      </div>
+    </nav>
+  );
 };
